99db63d416e8c4a323a38f1de366593a30697b05
hansel-sequeira <sequeirahansel@gmail.com>
2025-09-16 22:13:24 +0530

    lists added notes

--- 4_Lists/src/main/java/org/example/Main.java
+++ 4_Lists/src/main/java/org/example/Main.java
@@ -8,19 +8,22 @@ List interface
 
                // Collections.synchronizedList(new LinkedList<>())
                List<Integer> list = Collections.synchronizedList(new LinkedList<>());
-
                Can then, only use the List methods and not the Deque methods.
-
 
     -> Vector // existed before ArrayList. Is thread-safe.
         -> Stack // also thread-safe.
+
     -> ArrayList // Collections.synchronizedList(new ArrayList<>())
+
+    -> why doesn't the normal iterator provide the add() method, when it has the remove() method?
+            -> Because Iterator is meant for all Collection types (not Map!). For sets, priority queues, which are unordered, it doesn't make sense to have an add().
 
     certain methods in List: add(idx, element), addAll(idx, Collection), set(idx, element),
     indexOf(element), lastIndexOf(element), remove(idx / elt), sort(Comparator<T>),
     List<E> subList(fromIndex -> inclusive, toIndex -> exclusive)
-    * Any change in sublist will change the main list too!
-        -> WHY?
+    * Any change in sublist will change the main list too! Sublist is not a new list created in memory. It's a 'view' of the internal backing primary list.
+        -> sublist doesn't create any list. Sublist is an inner class of List. When you do sublist(0, 10) -> internally, sublist calls the same
+        get(), add() methods of the parentList, i.e ArrayList - it just does arithmetic computation for the offset (size, idx = endRange-startRage , etc).
  */
 
 
@@ -28,17 +31,25 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        arrayList.remove(2);
-        arrayList.remove(Integer.valueOf(2));
-        arrayList.sort((Integer a, Integer b) -> a-b);
 
-        ListIterator<Integer> listIterator = arrayList.listIterator(); // -> returns an object of ListIterator.
+        ArrayList<Integer> primaryList = new ArrayList<>(5); // no empty slots yet. Internally, Lists use Object[]/
+        // defining the initialCap just makes sure the internal backing Object array has that many slots at the minimum.
+        // but to iterate/set over the list, you need to prefill with null.
+
+        Collections.synchronizedList(new ArrayList<>());
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for(int i=0;i<10;i++)
+                arrayList.add(i);
+        List<Integer> subList = arrayList.subList(0, 2); // returns List<> type and does not create a deep copy.
+        subList.set(0, -100);
+        arrayList.forEach(elt -> System.out.println(elt)); // this mutates the original array.
+
+        arrayList.sort((Integer a, Integer b) -> a-b); // sort() is an exclusive method present in ArrayList which must take in a comparator.
+
+        ListIterator<Integer> listIterator = arrayList.listIterator(); // -> returns an object of ListIterator. (extends Iterator which is an interface).
         /*
             ListIterator in addition to having Iterator's next(), hasNext(), remove(), also provides:
-            previous(), hasPrevious(), add;
+            previous(), hasPrevious(), add();
          */
         while(listIterator.hasNext()) {
             int val = listIterator.next();
